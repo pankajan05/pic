85a4f150e7398c689b430f95b33cc96fb8509ee7
pankajan <pankajan05@gmail.com>
2021-04-10 16:11:09 +0530

    add grid system

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -1,10 +1,24 @@
 import react from 'react';
 
 class ImageCard extends react.Component{
+    constructor(props){
+        super(props);
+        this.state = {span : 0};
+        this.imageRef = react.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load',this.setSpan);
+    }
+
+    setSpan = () =>{
+        const span = Math.ceil(this.imageRef.current.clientHeight/10);
+        this.setState({span});
+    };
 
     render() {
-        return (<div>
-            <img src={this.props.image.urls.regular} alt={this.props.image.description}/>
+        return (<div style={{gridRowEnd: `span ${this.state.span}`}}>
+            <img  ref={this.imageRef} src={this.props.image.urls.regular} alt={this.props.image.description}/>
         </div>);
     }
 }

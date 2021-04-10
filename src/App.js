2ee0df20f3f078df591e0751e6fac366b5d5c123
pankajan <pankajan05@gmail.com>
2021-04-10 14:15:13 +0530

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import SearchBar from './Components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <SearchBar/>
-      </div>
-    </div>
-  );
+class App extends React.Component{
+    /*onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {
+                query: term
+            },
+            headers : {
+                Authorization: 'Client-ID Cq8pkgW8txs-LYlZU_ZXaOvfCuA8dLyKIrooYh9Z3WM'
+            }
+        }).then((response) => {
+            console.log(response.data.result);
+        });
+    }*/
+
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
+            params: {
+                query: term
+            },
+            headers : {
+                Authorization: 'Client-ID Cq8pkgW8txs-LYlZU_ZXaOvfCuA8dLyKIrooYh9Z3WM'
+            }
+        });
+        console.log(response.data.results);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <SearchBar onSubmit={this.onSearchSubmit}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,16 +8,19 @@ class SearchBar extends React.Component{
         this.setState({temp: event.target.value});
     };*/
 
-    onFormSubmit(event){
-        event.preventDefault();
-    }
+    onFormSubmit = (event) => {
+        event.preventDefault(); //prevent refresh when user press enter
+
+        this.props.onSubmit(this.state.temp);
+    };
 
     render(){
         return (
             <div className="ui segment center aligned basic">
+                <p style={{paddingRight:10}}>Image Search</p>
                 <form className="ui left icon input" onSubmit={this.onFormSubmit}>
                     <i className="search icon"></i>
-                    <input type="text" value={this.state.temp} onChange={(event => {this.setState({temp: event.target.value})})}/>
+                    <input type="text" value={this.state.temp} onChange={(e => {this.setState({temp: e.target.value})})}/>
                 </form>
             </div>);
     }
